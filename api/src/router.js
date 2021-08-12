7cc4007d87d03213e80ddfb894ffcae3f6cacb96
Tálisson Costa <talisson.odcosta@gmail.com>
2021-08-12 18:34:51 -0300

    Fix tests

--- api/src/router.js
+++ api/src/router.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const multer = require("multer");
+const path = require("path");
+const imageProcessor = require("./imageProcessor");
 
 const router = Router();
 
-const filename = (req, file, callback) => {
+const filename = (request, file, callback) => {
   callback(null, file.originalname);
 };
 
@@ -12,7 +14,7 @@ const storage = multer.diskStorage({
   filename,
 });
 
-const fileFilter = (req, file, callback) => {
+const fileFilter = (request, file, callback) => {
   if (file.mimetype !== "image/png") {
     request.fileValidationError = "Wrong file type";
     callback(null, false, new Error("Wrong file type"));
